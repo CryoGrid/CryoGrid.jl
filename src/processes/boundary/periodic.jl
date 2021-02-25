b7224e1922e67adfae4c9e833c1ae5e15b829e4c
Brian Groenke <brian.groenke@awi.de>
2021-02-25 11:17:40 +0100

    Add periodic boundary condition

---
+++ src/processes/boundary/periodic.jl
@@ -0,0 +1,12 @@
+struct Periodic{P,S,T} <: BoundaryProcess{P}
+    period::Float64"s"
+    amplitude::T
+    offset::T
+    Periodic{P,S}(period::Q, amplitude::T=one(T), offset::T=one(T)) where
+        {P<:SubSurfaceProcess,S<:BoundaryStyle,Q<:TimeQuantity,T} =
+        new{P,S,T}(uconvert(u"s",period) |> dustrip, amplitude, offset)
+end
+
+(bc::Periodic)(t) = bc.amplitude*sin(π*(1/bc.period)*t) + bc.offset
+
+BoundaryStyle(::Type{Periodic{P,S}}) where {P,S} = S()
