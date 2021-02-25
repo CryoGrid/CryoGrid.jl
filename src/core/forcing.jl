4f74e5d37a98d6c195ac2433c54fc0d4c2a68089
Brian Groenke <brian.groenke@awi.de>
2021-02-25 13:13:01 +0100

    Refactor Forcing types

--- src/core/forcing.jl
+++ src/core/forcing.jl
@@ -1,16 +1,23 @@
-struct Forcing{T,A}
+"""
+      Forcing{T,N}
+"""
+abstract type Forcing{T,N} end
+"""
+      TimeSeriesForcing{T,A}
+"""
+struct TimeSeriesForcing{T,A} <: Forcing{T,1}
+      t0::DateTime
       tarray::TimeArray{T,1,DateTime,A}
-      Forcing(values::A, timestamps::AbstractArray{DateTime,1},
-              name::Symbol=:forcing; interpolation=Linear) where
-              {T,A<:AbstractArray{T,1}} = begin
-              degree = interpolation()
-              ts = datetime2unix.(timestamps)
-              interp = interpolate((ts,), values, Gridded(degree))
-              new{T,typeof(interp)}(TimeArray(timestamps, interp, [name]))
+      TimeSeriesForcing(values::A, timestamps::AbstractArray{DateTime,1}, name::Symbol=:forcing; interp=Linear()) where
+            {T,A<:AbstractArray{T,1}} = begin
+            t0 = timestamps[1]
+            ts = float.(timestamps .- t0)
+            interp = interpolate((ts,), values, Gridded(interp))
+            new{T,typeof(interp)}(t0,TimeArray(timestamps, interp, [name]))
       end
 end
 
-Base.getindex(f::Forcing, t::DateTime) = f[datetime2unix(t)]
-Base.getindex(f::Forcing, t::Float64) = values(f.tarray)(t)
-Base.getindex(f::Forcing, ts::AbstractArray{DateTime,1}) = getindex.(f,datetime2unix.(ts))
-Base.getindex(f::Forcing, ts::AbstractArray{Float64,1}) = values(f.tarray).(ts)
+"""
+Get forcing at time t.
+"""
+(forcing::TimeSeriesForcing)(t) = values(forcing.tarray)(t) # extract interpolation and evaluate
