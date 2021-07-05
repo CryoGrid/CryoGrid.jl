6399e47542166e7f9cc02be1901cd53495898206
Brian Groenke <brian.groenke@awi.de>
2021-07-05 18:16:59 +0200

    Update tests for math/utils submodules

--- test/core/math_tests.jl
+++ test/core/math_tests.jl
@@ -25,7 +25,7 @@ end
 	k = 2.0 # constant diffusion
 	δ = x[2:end] .- x[1:end-1]
 	∂²y = zeros(length(y)-2)
-	lineardiffusion!(y,δ,k,∂²y)
+	lineardiffusion!(∂²y,y,δ,k)
 	@test allfinite(∂²y)
 	@test allequal(∂²y,k*d2y,atol=0.01)
 end
@@ -45,7 +45,38 @@ end
 	δx = x[2:end] .- x[1:end-1]
 	δxc = xc[2:end] .- xc[1:end-1]
 	out = zeros(length(y)-2)
-	nonlineardiffusion!(y,δxc,kₓ,δx[2:end-1],out)
+	nonlineardiffusion!(out,y,δxc,kₓ,δx[2:end-1])
 	@test allfinite(out)
 	@test allequal(out,d2y,atol=0.01)
+end
+
+@testset "∇" begin
+	# test 1: single arg
+	f(x) = x^2 + 2*x
+	∇f(x) = 2x + 2
+	∇f_gen = ∇(f, :x)
+	xs = -10:0.1:10
+	true_vals = [∇f(x) for x in xs]
+	gen_vals = [∇f_gen(x) for x in xs]
+	@test all(true_vals .≈ gen_vals)
+	# test 2: with extra args
+	f2(x,a,b) = a*x^2 + 2*x*b
+	∇f2(x,a,b) = 2x*a + 2*b
+	∇f2_gen = ∇(f2, :x)
+	xs = -1:0.1:1
+	as = -1:0.1:1
+	bs = -1:0.1:1
+	true_vals = [∇f2(x,a,b) for x in xs for a in as for b in bs]
+	gen_vals = [∇f2_gen(x,a,b) for x in xs for a in as for b in bs]
+	@test all(true_vals .≈ gen_vals)
+	# test 3: with extra args, different order
+	f3(a,x,b) = a*x^2 + 2*x*b
+	∇f3(a,x,b) = 2x*a + 2*b
+	∇f3_gen = ∇(f3, :x)
+	xs = -1:0.1:1
+	as = -1:0.1:1
+	bs = -1:0.1:1
+	true_vals = [∇f3(a,x,b) for x in xs for a in as for b in bs]
+	gen_vals = [∇f3_gen(a,x,b) for x in xs for a in as for b in bs]
+	@test all(true_vals .≈ gen_vals)
 end
