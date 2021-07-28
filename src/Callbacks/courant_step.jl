be38bf848dbc960189df5a5beb716a57340b63b0
Brian Groenke <brian.groenke@awi.de>
2021-07-28 18:04:49 +0200

    Add isfinite check to CFL callback

--- src/Callbacks/courant_step.jl
+++ src/Callbacks/courant_step.jl
@@ -1,17 +1,28 @@
 struct CFLHeatState{A}
     Δt::A
+    default_dt::Float64
 end
-function (fn::CryoGridCallbackFunction{<:CFLHeatState})(u,p,t)
+function (fn::CryoGridCallbackFunction{CFLHeatState{<:AbstractArray}})(u,p,t)
     let Δt = fn.state.Δt,
         Δx = Δ(fn.setup.grid),
         Ceff = getvar(:Ceff, fn.setup, u), # apparent heat capacity
         kc = getvar(:kc, fn.setup, u); # thermal cond. at grid centers
         @. Δt = adstrip(Δx^2 * Ceff / kc)
-        minimum(Δt)
+        Δt_min = minimum(Δt)
+        IfElse.ifelse(isfinite(Δt_min), Δt_min, fn.default_dt)
     end
 end
-function CFLStepLimiter(setup::HeatOnlySetup; courant_number=1//2)
-    state = CFLHeatState(similar(Δ(setup.grid)))
+function (fn::CryoGridCallbackFunction{CFLHeatState{Nothing}})(u,p,t)
+    let Δx = Δ(fn.setup.grid),
+        Ceff = getvar(:Ceff, fn.setup, u), # apparent heat capacity
+        kc = getvar(:kc, fn.setup, u); # thermal cond. at grid centers
+        Δt = adstrip(Δx^2 * Ceff / kc)
+        Δt_min = minimum(Δt)
+        IfElse.ifelse(isfinite(Δt_min), Δt_min, fn.default_dt)
+    end
+end
+function CFLStepLimiter(setup::HeatOnlySetup; courant_number=1//2, default_dt=60.0, iip=true)
+    state = iip ? CFLHeatState(similar(Δ(setup.grid)), default_dt) : CFLHeatState(nothing, default_dt)
     fn = CryoGridCallbackFunction(setup, state)
     StepsizeLimiter(fn; safety_factor=courant_number, max_step=true)
 end
