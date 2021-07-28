f38903134a1c3ad4f6ffd3e86f69a2874d4ef4fa
Brian Groenke <brian.groenke@awi.de>
2021-07-28 21:15:36 +0200

    Move DateTime forcing call overload to base type

--- src/Common/Forcings/Forcings.jl
+++ src/Common/Forcings/Forcings.jl
@@ -11,6 +11,8 @@ export Forcing, TimeSeriesForcing
       Forcing{T,N}
 """
 abstract type Forcing{T,N} end
+(forcing::Forcing)(x::Number) = error("$(typeof(forcing)) not implemented")
+(forcing::Forcing)(t::DateTime) = forcing(ustrip(u"s", float(Dates.datetime2epochms(t))u"ms"))
 
 include("timeseries.jl")
 
