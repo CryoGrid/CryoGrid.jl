34188c65c93d48d946ad057353db340a3118457f
Brian Groenke <brian.groenke@awi.de>
2021-08-09 12:20:13 +0200

    Add second nfactor parameter

--- src/Processes/HeatConduction/heat_bc.jl
+++ src/Processes/HeatConduction/heat_bc.jl
@@ -8,25 +8,27 @@ end
 
 BoundaryStyle(::Type{<:TemperatureGradient}) = Dirichlet()
 
-struct NFactor{F,nf,nt} <: BoundaryProcess
+struct NFactor{F,nf1,nf2,nt} <: BoundaryProcess
     tgrad::TemperatureGradient{F}
-    factor::Float64
+    factor1::Float64
+    factor2::Float64
     threshold::Float64
-    NFactor(tgrad::TemperatureGradient{F}, factor::Float64=0.5, threshold::Float64=0.0, name::Symbol=:n) where {F} =
-        new{F,Symbol(name,:_factor),Symbol(name,:_thresh)}(tgrad, factor, threshold)
+    NFactor(tgrad::TemperatureGradient{F}, factor1::Float64=0.5, factor2::Float64=1.0, threshold::Float64=0.0, name::Symbol=:n) where {F} =
+        new{F,Symbol(name,:_factor1),Symbol(name,:_factor2),Symbol(name,:_thresh)}(tgrad, factor1, factor2, threshold)
 end
 
-@inline function (bc::NFactor{F,nf,nt})(l1,l2,p2,s1,s2) where {F,nf,nt}
-    let factor = s1.params[nf] |> getscalar,
+@inline function (bc::NFactor{F,nf1,nf2,nt})(l1,l2,p2,s1,s2) where {F,nf1,nf2,nt}
+    let factor1 = s1.params[nf1] |> getscalar,
+        factor2 = s1.params[nf2] |> getscalar,
         thresh = s1.params[nt] |> getscalar,
         Tair = bc.tgrad(l1,l2,p2,s1,s2),
         check = Tair - thresh,
-        factor_eff = (check <= zero(factor))*factor + (check > zero(factor));
+        factor_eff = (check <= zero(check))*factor1 + (check > zero(check))*factor2;
         factor_eff*Tair
     end
 end
 
-variables(::Top, bc::NFactor{F,nf,nt}) where {F,nf,nt} = (Parameter(nf, bc.factor, 0..1), Parameter(nt, bc.threshold))
+variables(::Top, bc::NFactor{F,nf1,nf2,nt}) where {F,nf1,nf2,nt} = (Parameter(nf1, bc.factor1, 0..1), Parameter(nf2, bc.factor2, 0..1), Parameter(nt, bc.threshold))
 BoundaryStyle(::Type{<:NFactor}) = Dirichlet()
 
 # Boundary condition type aliases
