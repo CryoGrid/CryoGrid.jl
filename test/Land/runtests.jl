81dbd24d720293139870404599709f0ab42287fc
Brian Groenke <brian.groenke@awi.de>
2021-12-12 17:51:50 +0100

    Fix broken test names

--- test/Land/runtests.jl
+++
@@ -1,4 +0,0 @@
-@testset "Strat model setup" begin
-    include("tile_tests.jl")
-    include("stratigraphy_tests.jl")
-end
---
+++ test/Strat/runtests.jl
@@ -0,0 +1,2 @@
+include("tile_tests.jl")
+include("stratigraphy_tests.jl")
