07e3bf93b690f76b2c447a05180277c435a477eb
Brian Groenke <brian.groenke@awi.de>
2022-06-06 22:32:07 +0200

    Bump version number

--- src/Physics/Hydrology/Hydrology.jl
+++ src/Physics/Hydrology/Hydrology.jl
@@ -1,14 +1,23 @@
 module Hydrology
 
-import CryoGrid: BoundaryStyle, diagnosticstep!, prognosticstep!, interact!, initialcondition!, variables
+import CryoGrid
 
-using ..Physics
+using CryoGrid
+using CryoGrid.Physics
 using CryoGrid.Numerics
 using CryoGrid.Numerics: nonlineardiffusion!
 
 using IfElse
 using ModelParameters
 using Unitful
+
+abstract type WaterFlowParameterization end
+
+Base.@kwdef struct WaterFlow{TPara<:WaterFlowParameterization,Tinit,TProp} <: CryoGrid.SubSurfaceProcess
+    para::TPara = BucketScheme()
+    prop::TProp = HydrologicalProperties()
+    init::Tinit = nothing # optional initialization scheme
+end
 
 include("water_bucket.jl")
 
