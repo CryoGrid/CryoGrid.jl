58b8a5ca32c35bf0ec7f8650b3d2edf9342e4886
Brian Groenke <brian.groenke@awi.de>
2022-08-17 13:02:57 +0200

    Fix bug in Sources tests

--- test/Physics/Sources/runtests.jl
+++ test/Physics/Sources/runtests.jl
@@ -6,8 +6,7 @@ include("../../types.jl")
 @testset "Sources" begin
     layer = TestGroundLayer()
     @testset "Constant" begin
-        heatsource = Source(Heat, Sources.Constant(Param(0.0, units=u"W/m^3")))
-        heatsource = CryoGrid.update(heatsource, [1.0u"W/m^3"])
+        heatsource = Source(Heat, Sources.Constant(1.0u"W/m^3"))
         state = (∂H∂t=zeros(100)u"W/m^3",)
         prognosticstep!(layer, heatsource, state)
         @test all(state.∂H∂t .≈ 1.0u"W/m^3")
@@ -17,13 +16,7 @@ include("../../types.jl")
     end
     @testset "Periodic" begin
         level, amp, freq, shift = 0.0u"W/m^3", 2.0u"W/m^3", 0.5u"Hz", π/2
-        heatsource = Source(Heat, Sources.Periodic(
-            level=Param(0.0, units=u"W/m^3"),
-            amp=Param(1.0, units=u"W/m^3"),
-            freq=Param(1.0, units=u"Hz"),
-            shift=Param(0),
-        ))
-        heatsource = CryoGrid.update(heatsource, [amp, freq, shift, level])
+        heatsource = Source(Heat, Sources.Periodic(; level, amp, freq, shift))
         state = (∂H∂t=zeros(100)u"W/m^3", t=1.0u"s")
         prognosticstep!(layer, heatsource, state)
         @test all(state.∂H∂t .≈ level + amp*sin(2π*freq*1.0u"s" - shift))
