03843d504e35ea2db63ab22459c9f1a12ba10dde
Moritz Langer <moritz.langer@awi.de>
2023-03-03 18:36:42 +0100

    lake thing
    Co-authored-by: Brian Groenke <brian.groenke@awi.de>

--- src/Physics/Lakes/Lakes.jl
+++ src/Physics/Lakes/Lakes.jl
@@ -26,6 +26,7 @@ CryoGrid.volumetricfractions(::Lake, state, i) = (state.θw[i], 1 - state.θw[i]
 
 CryoGrid.variables(lake::Lake, heat::HeatBalance) = (
     CryoGrid.variables(heat)...,
+    # Diagnostic(:I_t, OnGrid(Cells), desc="Indicator variable for is thawed (1 or 0)."),
     Diagnostic(:ρ_w, Scalar, u"kg*m^-3", domain=0..Inf, desc = "density of water with temperature"),
     Diagnostic(:T_ub, Scalar, u"°C"),
 )
@@ -57,11 +58,55 @@ function CryoGrid.diagnosticstep!(sub::Lake, heat::HeatBalance, state)
     return nothing
 end
 
+function CryoGrid.diagnosticstep!(
+    sub::Lake,
+    heat::HeatBalanceImplicit,
+    state
+)
+    Heat.resetfluxes!(sub, heat, state)
+    # Evaluate freeze/thaw processes
+    Heat.freezethaw!(sub, heat, state)
+    # Update thermal conductivity
+    Heat.thermalconductivity!(sub, heat, state)
+    isthawed = true
+    @inbounds for i in eachindex(state.θw)
+        isthawed = isthawed && state.θw[i] ≈ 1.0
+    end
+    I_t = Float64(isthawed)
+    # Compute diffusion coefficients
+    an = state.DT_an
+    as = state.DT_as
+    ap = state.DT_ap
+    k = state.k
+    dx = Δ(cells(state.grid))
+    dxp = Δ(state.grid)
+    k_inner = @view k[2:end-1]
+    dxpn = @view dxp[1:end-1]
+    dxps = @view dxp[2:end]
+    @. an[2:end] = k_inner / dx / dxpn
+    @. as[1:end-1] = (k_inner / dx / dxps)*I_t
+    @. ap[1:end-1] += as[1:end-1]
+    @. ap[2:end] += an[2:end]
+    @. an[2:end] *= I_t
+    return nothing
+end
+
+CryoGrid.prognosticstep!(::Lake, ::HeatBalanceImplicit, state) = nothing
+
 function CryoGrid.prognosticstep!(::Lake, ::HeatBalance{<:FreezeCurve,<:Heat.Enthalpy}, state)
     Δk = Δ(state.grids.k) # cell sizes
     ΔT = Δ(state.grids.T) # midpoint distances
     # compute internal fluxes and non-linear diffusion assuming boundary fluxes have been set
     Numerics.nonlineardiffusion!(state.∂H∂t, state.jH, state.T, ΔT, state.k, Δk)
+    return nothing
+end
+
+function CryoGrid.interact!(top::Top, bc::HeatBC, lake::Lake, heat::HeatBalanceImplicit, stop, slake)
+    Δk = CryoGrid.thickness(lake, slake, first)
+    jH_top = boundaryflux(bc, top, heat, lake, stop, slake)
+    k = slake.k[1]
+    slake.DT_bp[1] += jH_top / Δk
+    slake.DT_ap[1] += Heat._ap(CryoGrid.BoundaryStyle(bc), k, Δk)
     return nothing
 end
 
