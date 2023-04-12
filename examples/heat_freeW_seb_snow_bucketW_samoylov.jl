8b4c6821aeb4652f6a0b6f4963d8bbb09abacaee
Brian Groenke <brian.groenke@awi.de>
2023-04-12 14:41:01 +0200

    Update example script for SEB/snow/water

--- examples/heat_freeW_seb_snow_bucketW_samoylov.jl
+++ examples/heat_freeW_seb_snow_bucketW_samoylov.jl
@@ -1,10 +1,21 @@
 using CryoGrid
+using Dates
 using Plots
 
-# Custom grid (though actually pretty much the same as CryoGrid.Presets.DefaultGrid_2cm)
-const gridvals = vcat([0:0.02:2...,2.05:0.05:4.0...,
-	4.1:0.1:10...,10.2:0.2:20...,21:1:30...,
-	35:5:50...,60:10:100...,200:100:1000...]...)u"m"
+# Custom grid;
+# Similar to Presets.DefaultGrid_2cm but includes one large cell above the surface for snow.
+const gridvals = vcat([
+    -2.0,
+    0:0.02:2...,
+    2.05:0.05:4.0...,
+	4.1:0.1:10...,
+    10.2:0.2:20...,
+    21:1:30...,
+	35:5:50...,
+    60:10:100...,
+    200:100:1000...]...
+)u"m"
+modelgrid = Grid(gridvals);
 # soil profile: depth => (excess ice, natural porosity, saturation, organic fraction)
 soilprofile = SoilProfile(
     0.0u"m" => HomogeneousMixture(por=0.80,sat=1.0,org=0.75), #(θwi=0.80,θm=0.05,θo=0.15,ϕ=0.80),
@@ -22,6 +33,8 @@ forcings = loadforcings(
     :wind => u"m/s",
     :Lin => u"W/m^2",
     :Sin => u"W/m^2",
+    :snowfall => u"mm/d",
+    :rainfall => u"mm/d",
 );
 Tair = TimeSeriesForcing(forcings.data.Tair, forcings.timestamps, :Tair);
 pr   = TimeSeriesForcing(forcings.data.pressure, forcings.timestamps, :pr);
@@ -29,32 +42,52 @@ q    = TimeSeriesForcing(forcings.data.q, forcings.timestamps, :q);
 wind = TimeSeriesForcing(forcings.data.wind, forcings.timestamps, :wind);
 Lin  = TimeSeriesForcing(forcings.data.Lin, forcings.timestamps, :Lin);
 Sin  = TimeSeriesForcing(forcings.data.Sin, forcings.timestamps, :Sin);
+snowfall = TimeSeriesForcing(uconvert.(u"m/s", forcings.data.snowfall).*1.0, forcings.timestamps, :sf)
+rainfall = TimeSeriesForcing(uconvert.(u"m/s", forcings.data.rainfall).*1.0, forcings.timestamps, :rf)
 z = 2.;    # height [m] for which the forcing variables (Temp, humidity, wind, pressure) are provided
 soilprofile, tempprofile = CryoGrid.Presets.SamoylovDefault
 initT = initializer(:T, tempprofile)
+# initialize saturation to match soil profile
+initsat = initializer(:sat, (l,p,state) -> state.sat .= l.para.sat)
 seb = SurfaceEnergyBalance(Tair, pr, q, wind, Lin, Sin, z)
-# @Stratigraphy macro lets us list multiple subsurface layers
+subsurface_layers = map(enumerate(soilprofile)) do (i, soil_i)
+    name = Symbol(:soil, i)
+    heat = HeatBalance(:H, freezecurve=PainterKarra())
+    water = WaterBalance(BucketScheme(), DampedET())
+    soil_i.depth => name => Soil(soil_i.value; heat, water)
+end
+# build stratigraphy
 strat = @Stratigraphy(
-    -z*u"m" => Top(seb),
-    soilprofile[1].depth => :soil1 => Soil(soilprofile[1].value, heat=HeatBalance(:H, freezecurve=DallAmico())),
-    soilprofile[2].depth => :soil2 => Soil(soilprofile[2].value, heat=HeatBalance(:H, freezecurve=DallAmico())),
-    soilprofile[3].depth => :soil3 => Soil(soilprofile[3].value, heat=HeatBalance(:H, freezecurve=DallAmico())),
-    soilprofile[4].depth => :soil4 => Soil(soilprofile[4].value, heat=HeatBalance(:H, freezecurve=DallAmico())),
-    soilprofile[5].depth => :soil5 => Soil(soilprofile[5].value, heat=HeatBalance(:H, freezecurve=DallAmico())),
+    -z*u"m" => Top(seb, Rainfall(rainfall), Snowfall(snowfall)),
+    0.0u"m" => :snowpack => Snowpack(heat=HeatBalance()),
+    subsurface_layers...,
     1000.0u"m" => Bottom(GeothermalHeatFlux(0.053u"J/s/m^2")),
 );
-grid = Grid(gridvals);
-tile = Tile(strat, grid, initT);
+# create Tile
+tile = Tile(strat, modelgrid, initT, initsat);
+
 # define time span
 tspan = (DateTime(2010,10,30), DateTime(2011,10,30))
+# generate initial condition and set up CryoGridProblem
 u0, du0 = initialcondition!(tile, tspan)
-# CryoGrid front-end for ODEProblem; note that we disable CryoGrid's timestep limiter since we use an adaptive integrator
-prob = CryoGridProblem(tile, u0, tspan, savevars=(:T,:C,:∂H∂T,:top => (:Qh,:Qe,:Qg,)), step_limiter=nothing, saveat=3*3600.0)
-# solve with 3rd order Strong Stability Preserving Runge-Kutta
-sol = @time solve(prob, SSPRK43(), saveat=3*3600.0, progress=true)
-out = CryoGridOutput(sol)
+prob = CryoGridProblem(tile, u0, tspan, savevars=(:T,:top => (:Qh,:Qe,:Qg,),:snowpack => (:dsn,)), step_limiter=nothing, saveat=3*3600.0)
+# initialize integrator
+integrator = init(prob, Euler(), dt=120.0, progress=true)
+# take one 24 hour step to check that everything is working
+@time step!(integrator, 24*3600)
+@assert all(isfinite.(integrator.u))
+# iterate over remaining timespan
+@time for (u,t) in TimeChoiceIterator(integrator, convert_t.(tspan[1]:Day(1):tspan[end]))
+    # print once per day to track progress
+    @show convert_t(t)
+end
+# build output from solution
+out = CryoGridOutput(integrator.sol)
 # Plot it!
-zs = [1,5,10,15,20,25,30,40,50,]u"cm"
+zs = [1,5,10,15,20,25,30,40,50]u"cm"
 cg = Plots.cgrad(:copper,rev=true);
 plot(ustrip.(out.H[Z(Near(zs))]), color=cg[LinRange(0.0,1.0,length(zs))]', ylabel="Enthalpy", leg=false, dpi=150)
 plot(ustrip.(out.T[Z(Near(zs))]), color=cg[LinRange(0.0,1.0,length(zs))]', ylabel="Temperature", leg=false, size=(800,500), dpi=150)
+plot(ustrip.(out.sat[Z(Near(zs))]), color=cg[LinRange(0.0,1.0,length(zs))]', ylabel="Soil saturation", leg=false, size=(800,500), dpi=150)
+plot(ustrip.(out.snowpack.dsn), color=cg[LinRange(0.0,1.0,length(zs))]', ylabel="Snow depth", leg=false, size=(800,500), dpi=150)
+plot(ustrip.(cumsum(out.top.Qg, dims=2)), color=cg[LinRange(0.0,1.0,length(zs))]', ylabel="Integrated ground heat flux", leg=false, size=(800,500), dpi=150)
