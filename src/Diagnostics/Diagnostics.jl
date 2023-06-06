15c631ba80e510c689463e3fc9aff53bc1d7133d
Brian Groenke <brian.groenke@awi.de>
2023-06-06 23:43:49 +0200

    Add soft dependency w/ source file for plots.jl

--- src/Diagnostics/Diagnostics.jl
+++ src/Diagnostics/Diagnostics.jl
@@ -24,12 +24,16 @@ include("permafrost.jl")
 export spinup
 include("spinup.jl")
 
-include("viz/cgplots.jl")
+include("viz/cgviz.jl")
 
 function __init__()
     @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" begin
         import .Makie
-        include("viz/cgplots_makie.jl")
+        include("viz/cgviz_makie.jl")
+    end
+    @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
+        import .Plots
+        include("viz/cgviz_plots.jl")
     end
 end
 
