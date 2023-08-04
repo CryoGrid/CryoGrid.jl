fd8c320a45c2a77776cc0a176b4728e904782022
Brian Groenke <brian.groenke@awi.de>
2023-08-04 15:02:02 +0200

    Fix docs and add missing file

---
+++ src/Solvers/DiffEq/ode_solvers.jl
@@ -0,0 +1,13 @@
+# solve/init interface
+function DiffEqBase.__solve(prob::CryoGridProblem, alg::Union{OrdinaryDiffEqAlgorithm, OrdinaryDiffEq.DAEAlgorithm}, args...; saveat=prob.saveat, kwargs...)
+    ode_prob = ODEProblem(prob)
+    return DiffEqBase.solve(ode_prob, alg, args...; saveat, kwargs...)
+end
+function DiffEqBase.__init(prob::CryoGridProblem, alg::Union{OrdinaryDiffEqAlgorithm, OrdinaryDiffEq.DAEAlgorithm}, args...; saveat=prob.saveat, kwargs...)
+    ode_prob = ODEProblem(prob)
+    return DiffEqBase.init(ode_prob, alg, args...; saveat, kwargs...)
+end
+
+# custom nonlinear solvers
+export NLCGLite
+include("nlsolve/nlsolve.jl")
