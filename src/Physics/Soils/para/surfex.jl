88a8de79f8e516ed79a5d5551af0dc9ddbe5670f
Brian Groenke <brian.groenke@awi.de>
2024-05-11 12:44:13 +0200

    Rename SURFEX -> SURFEXSoil

--- src/Physics/Soils/para/surfex.jl
+++ src/Physics/Soils/para/surfex.jl
@@ -1,5 +1,5 @@
-# SURFEX paramterization
-Base.@kwdef struct SURFEX{Tρs,Tρo,Tpo,Tsat,Twilt,Ttex<:SoilTexture,Thp,Twp} <: SoilParameterization
+# SURFEXSoil paramterization
+Base.@kwdef struct SURFEXSoil{Tρs,Tρo,Tpo,Tsat,Twilt,Ttex<:SoilTexture,Thp,Twp} <: SoilParameterization
     ρ_soc::Tρs = 65.0u"kg/m^3"
     ρ_org::Tρo = 1300u"kg/m^3"
     por_org::Tpo = 0.90
@@ -10,41 +10,41 @@ Base.@kwdef struct SURFEX{Tρs,Tρo,Tpo,Tsat,Twilt,Ttex<:SoilTexture,Thp,Twp} <:
     water::Twp = HydraulicProperties(fieldcapacity=0.20) # hydraulic properties
 end
 
-# SURFEX functions
-organic(surf::SURFEX) = surf.ρ_soc / ((1 - surf.por_org)*surf.ρ_org)
+# SURFEXSoil functions
+organic(surf::SURFEXSoil) = surf.ρ_soc / ((1 - surf.por_org)*surf.ρ_org)
 
-mineral_porosity(surf::SURFEX) = 0.49 - 0.11*surf.texture.sand
+mineral_porosity(surf::SURFEXSoil) = 0.49 - 0.11*surf.texture.sand
 
-mineral_wilting_point(surf::SURFEX) = 0.37*sqrt(surf.texture.clay)
+mineral_wilting_point(surf::SURFEXSoil) = 0.37*sqrt(surf.texture.clay)
 
-mineral_field_capacity(surf::SURFEX) = 0.45*surf.texture.clay^0.3496
+mineral_field_capacity(surf::SURFEXSoil) = 0.45*surf.texture.clay^0.3496
 
 # Soil methods
-organic(soil::Soil{<:SURFEX}) = organic(soil.para)
+organic(soil::Soil{<:SURFEXSoil}) = organic(soil.para)
 
-mineral(soil::Soil{<:SURFEX}) = 1 - organic(soil.para)
+mineral(soil::Soil{<:SURFEXSoil}) = 1 - organic(soil.para)
 
-saturation(soil::Soil{<:SURFEX}) = soil.para.sat
+saturation(soil::Soil{<:SURFEXSoil}) = soil.para.sat
 
-function porosity(soil::Soil{<:SURFEX})
+function porosity(soil::Soil{<:SURFEXSoil})
     org = organic(soil)
     por = org*soil.para.por_org + (1-org)*mineral_porosity(soil.para)
     return por
 end
 
-CryoGrid.initializers(soil::Soil{<:SURFEX,THeat,<:WaterBalance}) where {THeat} = (
+CryoGrid.initializers(soil::Soil{<:SURFEXSoil,THeat,<:WaterBalance}) where {THeat} = (
     initializer(:sat, soil.para.sat),
     initializers(soil, processes(soil))...,
 )
 
 # Heat
 
-function Heat.thermalconductivities(soil::Soil{<:SURFEX})
+function Heat.thermalconductivities(soil::Soil{<:SURFEXSoil})
     @unpack kh_w, kh_i, kh_a, kh_m, kh_o = thermalproperties(soil)
     return kh_w, kh_i, kh_a, kh_m, kh_o
 end
 
-function Heat.heatcapacities(soil::Soil{<:SURFEX})
+function Heat.heatcapacities(soil::Soil{<:SURFEXSoil})
     @unpack ch_w, ch_i, ch_a, ch_m, ch_o = thermalproperties(soil)
     return ch_w, ch_i, ch_a, ch_m, ch_o
 end
@@ -52,11 +52,11 @@ end
 """
 Gets the `ThermalProperties` for the given soil layer.
 """
-Heat.thermalproperties(soil::Soil{<:SURFEX}) = soil.para.heat
+Heat.thermalproperties(soil::Soil{<:SURFEXSoil}) = soil.para.heat
 
 # Soil thermal properties
 SoilThermalProperties(
-    ::Type{SURFEX};
+    ::Type{SURFEXSoil};
     kh_w = ThermalProperties().kh_w,
     kh_i = ThermalProperties().kh_i,
     kh_a = ThermalProperties().kh_a,
@@ -75,13 +75,13 @@ SoilThermalProperties(
 """
 Gets the `HydraulicProperties` for the given soil layer.
 """
-Hydrology.hydraulicproperties(soil::Soil{<:SURFEX}) = soil.para.water
+Hydrology.hydraulicproperties(soil::Soil{<:SURFEXSoil}) = soil.para.water
 
 # field capacity
-Hydrology.minwater(soil::Soil{<:SURFEX}, ::WaterBalance) = hydraulicproperties(soil).fieldcapacity
+Hydrology.minwater(soil::Soil{<:SURFEXSoil}, ::WaterBalance) = hydraulicproperties(soil).fieldcapacity
 
 # porosity/max water
-Hydrology.maxwater(soil::Soil{<:SURFEX}, water::WaterBalance) = porosity(soil)
+Hydrology.maxwater(soil::Soil{<:SURFEXSoil}, water::WaterBalance) = porosity(soil)
 
 # water content for soils without water balance
-Hydrology.watercontent(soil::Soil{<:SURFEX,THeat,Nothing}, state) where {THeat} = porosity(soil)*saturation(soil, state)
+Hydrology.watercontent(soil::Soil{<:SURFEXSoil,THeat,Nothing}, state) where {THeat} = porosity(soil)*saturation(soil, state)
