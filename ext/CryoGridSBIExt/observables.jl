61120ef5ba99e3c9d7894788031dd46e5ecc5475
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-04-04 19:34:51 +0200

    Fix outdated docstrings in SBI ext

--- ext/CryoGridSBIExt/observables.jl
+++ ext/CryoGridSBIExt/observables.jl
@@ -1,19 +1,23 @@
 """
     TemperatureProfileObservable(name::Symbol, zs::AbstractVector, t0::DateTime, tsave::AbstractVector{DateTime}; kwargs...)
 
-Special case of `TileStateObservable` for temperature profiles.
+Create a `SimulatorObservable` that records temperature profiles at depths `zs` over `tspan`.
 """
 function TemperatureProfileObservable(name::Symbol, zs::AbstractVector, tspan::NTuple{2,DateTime}, p::Period; kwargs...)
     SimulatorObservable(
         integrator -> collect(map(getvar(Val{:T}(), Tile(integrator), integrator.u; interp=true), ustrip.(zs))),
         (length(zs),),
         name = name,
-        output = TimeSampled(tspan[1], tspan[1]+p:p:tspan[2]),
-        time_converter = CryoGrid.convert_t,
+        output = TimeSampled(tspan[1], tspan[1]+p:p:tspan[2], time_converter=CryoGrid.convert_t),
         kwargs...
     )
 end
 
+"""
+    ActiveLayerThicknessObservable(name::Symbol, tspan::NTuple{2,DateTime}; samplerate=Hour(12), kwargs...)
+
+Create a `SimulatorObservable` that records annual active layer thickness over `tspan`.
+"""
 function ActiveLayerThicknessObservable(name::Symbol, tspan::NTuple{2,DateTime}; samplerate=Hour(12), kwargs...)
     @assert Date(tspan[2]) - Date(tspan[1]) >= Day(365) "Active layer thickness requires tspan of >= 1 year"
     return SimulatorObservable(
@@ -24,13 +28,18 @@ function ActiveLayerThicknessObservable(name::Symbol, tspan::NTuple{2,DateTime};
             tspan[1]+samplerate,
             tspan[1]+Year(1):Year(1):tspan[2],
             samplerate=samplerate,
-            time_converter=CryoGrid.convert_t
+            time_converter=CryoGrid.convert_t,
+            reducer = maximum,
         ),
-        reducer = maximum,
         kwargs...
     )
 end
 
+"""
+    LayerVarObservable(name::Symbol, layername::Symbol, varname::Symbol, grid::Grid, tspan::NTuple{2,DateTime}, p::Period=Day(1); kwargs...)
+
+Create a `SimulatorObservable` for the state variable `varname` defined on `layername`.
+"""
 function LayerVarObservable(name::Symbol, layername::Symbol, varname::Symbol, grid::Grid, tspan::NTuple{2,DateTime}, p::Period=Day(1); kwargs...)
     SimulatorObservable(
         integrator -> getproperty(getproperty(getstate(integrator), layername), varname),
